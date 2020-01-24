d4b1fe66235ba83a564a0e48677bc1536eb1f69e
Sabrina Becker <sbecka8@gmail.com>
2020-01-24 11:22:23 -0500

    Learning about response object and content-type

--- app.js
+++ app.js
@@ -11,6 +11,74 @@ app.use(morgan('dev'));
 //app.get(PATH, HANDLER);
 //HANDLER = (requestObject, responseObject)
 
+app.get('/hello', (req, res) => {
+    res.status(200).send('Hello, everything was ok!');
+});
+
+app.get('/andioop', (req, res) => {
+    res.status(204).end(); //end request and response is sent back to client
+});
+
+app.get('/video', (req, res) => {
+    const video = {
+        title: 'Cats falling over',
+        description: '12 minutes of hilarious fun as cats fall over',
+        length: '15.40'
+    };
+
+    res.json(video);
+});
+
+app.get('/colors', (req, res) => {
+    const colors = [
+        {
+            name: "red",
+            rgb: "FF0000"
+        },
+        {
+            name: "green",
+            rgb: "00FF00"
+        },
+        {
+            name: "blue",
+            rgb: "0000FF"
+        }
+    ];
+    res.json(colors);
+});
+
+app.get('/grade', (req, res) => {
+    const { mark } = req.query;
+
+    if (!mark) {
+        return res.status(400).send('Please provide a mark');
+    };
+
+    const numericMark = parseFloat(mark);
+
+    if (Number.isNaN(numericMark)) {
+        return res.status(400).send('Mark must be a numeric value');
+    };
+    
+    if (numericMark < 0 || numericMark > 100) {
+        return res.status(400).send('Mark must be in a range 0 to 100');
+    };
+
+    if (numericMark >= 90) {
+        return res.send('A');
+    }
+
+    if (numericMark >= 80) {
+        return res.send('B');
+    }
+
+    if (numericMark >= 70) {
+        return res.send('C');
+    }
+    
+    res.send('F');
+});
+
 // Drill 1 Return sum of two numbers
 app.get('/sum', (req, res) => {
     const a = parseInt(req.query.a);
